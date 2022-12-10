de08a72d24795789ca1ef1476702453db9aa1897
Asya Klimenko <68095775+Anisia-Klimenko@users.noreply.github.com>
2022-12-10 20:37:01 +0300

    feat: endpoints (#1)
    * Added First Functions
    
    * feat: server launch
    
    * feat: database 3 instances
    
    Co-authored-by: Connington Horse <chorse@su-j5.msk.21-school.ru>
    Co-authored-by: margosq <slava1kvartovkin@gmail.com>

---
+++ database/warehouse.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"context"
+
+	protos "github.com/Anisia-Klimenko/gRPC_golang_21school/protos/warehouse"
+)
+
+// Currency is a gRPC server it implements the methods defined by the CurrencyServer interface
+type Warehouse struct {
+	// log hclog.Logger
+}
+
+// NewCurrency creates a new Currency server
+func NewWarehouse() *Warehouse {
+	return &Warehouse{}
+}
+
+// GetRate implements the CurrencyServer GetRate method and returns the currency exchange rate
+// for the two given currencies.
+func (c *Warehouse) GetItem(ctx context.Context, rr *protos.UUID) (*protos.GetItemResponse, error) {
+	data := "kek"
+
+	return &protos.GetItemResponse{Name: data}, nil
+}
+
+func (c *Warehouse) SetItem(ctx context.Context, rr *protos.UUID) (*protos.OperationResultResponse, error) {
+	data := "kek"
+
+	return &protos.OperationResultResponse{Msg: data}, nil
+}
+
+func (c *Warehouse) DeleteItem(ctx context.Context, rr *protos.UUID) (*protos.OperationResultResponse, error) {
+	data := "kek"
+
+	return &protos.OperationResultResponse{Msg: data}, nil
+}
