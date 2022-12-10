1d13f343198e1b65074979950dc2bc340ddf4d64
margosq <slava1kvartovkin@gmail.com>
2022-12-10 19:12:31 +0300

    feat: server launch

--- database/warehouse.go
+++ database/warehouse.go
@@ -1,9 +1,9 @@
-package server
+package database
 
 import (
-	"context"
 
-	protos "github.com/Anisia-Klimenko/gRPC_golang_21school/protos/warehouse"
+	// protos "github.com/Anisia-Klimenko/gRPC_golang_21school/protos/warehouse"
+
 	"github.com/hashicorp/go-hclog"
 )
 
@@ -12,6 +12,8 @@ type Warehouse struct {
 	log hclog.Logger
 }
 
+
+
 // NewCurrency creates a new Currency server
 func NewWarehouse(l hclog.Logger) *Warehouse {
 	return &Warehouse{l}
@@ -19,7 +21,7 @@ func NewWarehouse(l hclog.Logger) *Warehouse {
 
 // GetRate implements the CurrencyServer GetRate method and returns the currency exchange rate
 // for the two given currencies.
-func (c *Warehouse) GetItem(ctx context.Context, rr *protos.UUID) (*protos.GetItemResponse, error) {
-	c.log.Info("Handle request for GetRate", "base", rr.GetBase(), "dest", rr.GetDestination())
-	return &protos.GetItemResponse{Rate: 0.5}, nil
-}
+// func (c *Warehouse) GetItem(ctx context.Context, rr *protos.UUID) (*protos.GetItemResponse, error) {
+// 	c.log.Info("Handle request for GetRate", "base", rr.GetBase(), "dest", rr.GetDestination())
+// 	return &protos.GetItemResponse{}, nil
+// }
